feb7bd31a5a48bf05318f555ce44cf1c5c6011e8
gilzcdng <pyorba6@gmail.com>
2025-05-19 10:25:29 -0700

    Updated a little bit of code still need the resume function as well.
    Added a reset button still get bugs where if I click it while timer is still active you get two timer need to fix, as well as need to create the resume function for when the timer is stopped.

--- pomodoro.js
+++ pomodoro.js
@@ -4,7 +4,7 @@ document.getElementById("startBtn").addEventListener('click', () =>{
 // Set the countdown time in minutes
 
 
-let startingMinutes = 1;
+let startingMinutes = 50;
 let time = startingMinutes * 60;
 let intervalId = null;
 
@@ -21,7 +21,7 @@ function updateCountdown() {
 
     if (time < 0) {
         clearInterval(refreshIntervalId);
-        countdownEl.innerHTML = "00:00";
+        countdownEl.innerHTML = "Break Time!";
     }
 }
 
@@ -30,13 +30,46 @@ function updateCountdown() {
 document.getElementById("stopBtn").addEventListener('click', () =>{
     for (let i = 0; i < 100; i++) {
         clearInterval(i);
-    } 
+
+    document.getElementById("startBtn").textContent = "Resume?";
+    }
+
+    const countdownEl = document.getElementById("display");
+
+    
+    if(document.getElementById("startBtn").addEventListener('click', () =>{
+
+        console.log("Resumed");
 
 
 
+    }))
+1000});
+ 
+// Reset Timer
+// FIXME: This is not working as expected 
+// It should reset the timer to the initial value and stop the countdown not create a second new one!
 
+document.getElementById("resetBtn").addEventListener('click', () =>{
 
+let startingMinutes = 50;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById("display");
+
+let refreshIntervalId = setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+}
+
+    if (time < 0) {
+        clearInterval(refreshIntervalId);
+        countdownEl.innerHTML = "Break Time!";
+    }
 });
-
-
 
